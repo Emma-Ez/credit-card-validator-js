12bf3f8dec5919a33335a0e6a2914055641f50bf
Emma-Ez <emma.favoured@yahoo.com>
2024-02-13 14:40:14 +0100

    A more flexible card check logic added

--- ccValidator.js
+++ ccValidator.js
@@ -3,25 +3,8 @@ function validateCard(cardNumber) {
     // Remove any non-digit characters
     cardNumber = cardNumber.replace(/\D/g, '');
 
-    // Check if the card number consists of 16 digits
-    if (!/^\d{16}$/.test(cardNumber)) {
-        return false;
+// Check length based on card type
+const cardType = identifyCardType(cardNumber);
+    if (!cardType || cardNumber.length !== expectedLengths[cardType]) {
+      return false;
     }
-
-    // Apply Luhn algorithm
-    let sum = 0;
-    for (let i = 0; i < cardNumber.length; i++) {
-        let digit = parseInt(cardNumber[i]);
-
-        if (i % 2 === 0) {
-            digit *= 2;
-            if (digit > 9) {
-                digit -= 9;
-            }
-        }
-
-        sum += digit;
-    }
-
-    return sum % 10 === 0;
-}
