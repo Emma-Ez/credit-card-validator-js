931e185d3904bdced813e7fe7f0c6e817fd5f013
Emma-Ez <emma.favoured@yahoo.com>
2024-02-13 14:55:20 +0100

    Luhm algorithm updated

--- ccValidator.js
+++ ccValidator.js
@@ -8,3 +8,20 @@ const cardType = identifyCardType(cardNumber);
     if (!cardType || cardNumber.length !== expectedLengths[cardType]) {
       return false;
     }
+
+// Applying Luhn Algorithm
+let sum = 0;
+for (let i = cardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(cardNumber[i]);
+      if ((cardNumber.length - i) % 2 === 0) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      sum += digit;
+    }
+  
+    return sum % 10 === 0;
+}
+  
